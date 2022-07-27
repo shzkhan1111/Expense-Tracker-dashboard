4c51578ea9dfaeb1d1e2e4fb4d110fb19e75ca4e
Hammad Mehmood <hmahmood@syntagium.com.au>
2022-07-27 12:50:30 +0500

    Add project files.

---
+++ Expense Tracker/Models/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Transaction> Transactions{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+
+}
