6e2c13b9d5e7169c73839f2ba7d4a60bbc7788f9
Hammad Mehmood <hmahmood@syntagium.com.au>
2022-07-31 16:56:15 +0500

    Designing Form template

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +18,7 @@ namespace Expense_Tracker.Models
         public string Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+        
 
     }
 }
